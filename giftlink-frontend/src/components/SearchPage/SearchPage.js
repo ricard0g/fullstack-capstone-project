498c0a2ba88f6b1d0d97de1ec1e520f37538e26a
“ricard0g” <ricardoguzman444@gmail.com>
2024-10-17 12:28:34 -0400

    Implement search page component task 2 finished

--- giftlink-frontend/src/components/SearchPage/SearchPage.js
+++ giftlink-frontend/src/components/SearchPage/SearchPage.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {urlConfig} from '../../config';
 
 function SearchPage() {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [ageRange, setAgeRange] = useState(6);
+    const [searchResults, setSearchResults] = useState([]);
 
     //Task 1: Define state variables for the search query, age range, and search results.
     const categories = ['Living', 'Bedroom', 'Bathroom', 'Kitchen', 'Office'];
@@ -32,6 +35,26 @@ function SearchPage() {
 
 
     // Task 2. Fetch search results from the API based on user inputs.
+    const handleSearch = async () => {
+        const baseUrl = `${urlConfig.backendUrl}/api/search`;
+        const queryParams = new URLSearchParams({
+            name: searchQuery,
+            age_years: ageRange,
+            category: document.getElementById('categorySelect').value,
+            condition: document.getElementById('conditionSelect').value,
+        }).toString();
+
+        try {
+            const response = await fetch(`${baseUrl}${queryParams}`);
+            if (!response.ok) {
+                throw new Error('Search failed');
+            }
+            const data = await response.json();
+            setSearchResults(data);
+        } catch(error) {
+            console.error("Failed to fetch search results:", error);
+        }
+    }
 
     const navigate = useNavigate();
 
