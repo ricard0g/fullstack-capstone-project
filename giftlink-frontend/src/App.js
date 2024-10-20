8fd278ef1ee345b601061d7aff32e53f1b7def1b
“ricard0g” <ricardoguzman444@gmail.com>
2024-10-20 06:42:03 -0400

    Implement search functionality and style searchPage

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -20,6 +20,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage />} />
         </Routes>
         </>
   );
