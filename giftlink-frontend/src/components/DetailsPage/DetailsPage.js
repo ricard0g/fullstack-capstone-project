f35205c8b504b04989601c107dd2d29bdc6d73f0
“ricard0g” <ricardoguzman444@gmail.com>
2024-10-21 11:48:24 -0400

    Linter corrections

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -39,7 +39,7 @@ function DetailsPage() {
         // Task 3: Scroll to top on component mount
         window.scrollTo(0, 0);
 
-    }, [productId]);
+    }, [productId, navigate]);
 
 
     const handleBackClick = () => {
