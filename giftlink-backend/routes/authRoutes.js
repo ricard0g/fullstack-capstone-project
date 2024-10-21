8100d92f2ed622b00b39838dde9fa67af5447139
“ricard0g” <ricardoguzman444@gmail.com>
2024-10-21 06:37:10 -0400

    Login route added backend

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -12,43 +12,75 @@ const logger = pino();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/register', async (req, res) => {
-    try {
-        const db = await connectToDatabase();
+router.post("/register", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
 
-        const collection = db.collection('users');
+    const collection = db.collection("users");
 
-        const existingEmail = await collection.findOne({ email: req.body.email });
+    const existingEmail = await collection.findOne({ email: req.body.email });
 
-        if (existingEmail) {
-            return res.status(400).json({ message: "This email is already signed in." });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(req.body.password, salt);
-        const email = req.body.email;
-
-        const newUser = await collection.insertOne({
-            email: email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: hash,
-            createdAt: new Date(),
-        })
-
-        const payload = {
-            user: {
-                id: newUser.insertedId,
-            },
-        };
-
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-
-        logger.info("User registered successfully");
-        res.json({ authtoken, email });
-    } catch (error) {
-        return res.status(500).json({ message: "Internal server error" })
+    if (existingEmail) {
+      return res
+        .status(400)
+        .json({ message: "This email is already signed in." });
     }
-})
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    const email = req.body.email;
+
+    const newUser = await collection.insertOne({
+      email: email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: hash,
+      createdAt: new Date(),
+    });
+
+    const payload = {
+      user: {
+        id: newUser.insertedId,
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+
+    logger.info("User registered successfully");
+    res.json({ authtoken, email });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+
+    const collection = db.collection("users");
+
+    const theUser = await collection.findOne({ email: req.body.email });
+
+    if (theUser) {
+      let result = await bcrypt.compare(req.body.password, theUser.password);
+      if (!result) {
+        logger.error("Passwords do not match");
+        return res.status(404).json({ error: "Wrong password" });
+      }
+      const userName = theUser.firstName;
+      const userEmail = theUser.email;
+
+      let payload = {
+        user: {
+          id: theUser._id.toString(),
+        },
+      };
+      jwt.sign(user._id, JWT_SECRET);
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {}
+});
 
 module.exports = router;
